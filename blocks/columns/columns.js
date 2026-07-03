328e1c9682494498fbf66b211c80673e2552dc4c
Prafful Singhal <praffuls@Praffuls-MacBook-Pro.local>
2026-07-03 13:04:16 +0530

    code update for column block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,8 +2,18 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  if (block.closest('main')) {
+    block.querySelectorAll('a[href]').forEach((link) => {
+      link.classList.add('button', 'primary');
+      const paragraph = link.closest('p');
+      if (paragraph) paragraph.classList.add('button-wrapper');
+    });
+  }
+
   // setup image columns
   [...block.children].forEach((row) => {
+    if (row.children.length > 2) row.classList.add('columns-icon-row');
+
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
